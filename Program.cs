e51a3fcb4d28bf50fd3847e6958ba80924dabd34
Gloomy-stack <karl.yotl@gmail.com>
2021-02-27 15:25:23 +0100

    Added main code and "GetNumber" method

--- Program.cs
+++ Program.cs
@@ -10,7 +10,32 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-
+            var Game = new FizzBuzz();
+            Console.WriteLine("Podanie wartości 'q' kończy działanie programu");
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę:");
+                var msg = Game.Check(GetNumber());
+                Console.WriteLine(msg);
+            }
+        }
+        public static int GetNumber()
+        {
+           
+            
+            while (true)
+            {
+                var val = Console.ReadLine();
+                if (val == "q")
+                    Environment.Exit(0);
+                if (!int.TryParse(val, out int number))
+                    {
+                        Console.WriteLine("Podaj prawidłową liczbę!");
+                        continue;
+                    }
+                    return number;              
+            }
+            
         }
     }
 }
