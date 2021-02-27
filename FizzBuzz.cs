93f68b519729afce51c00fd89d9e4c99cb030592
Gloomy-stack <karl.yotl@gmail.com>
2021-02-27 14:43:19 +0100

    Added class "FizzBuzz" with method "Check"

---
+++ FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string Check(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return number.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
